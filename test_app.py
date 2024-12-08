ba7bcfb46a58421b615c6b5f9ba52c844ee02944
mehdikhoshpoush <mehdi.khoshpoosh@gmail.com>
2024-12-08 22:33:31 +0330

    Edit the test file

--- test_app.py
+++ test_app.py
@@ -3,7 +3,7 @@ from app import hello_world
 
 class TestApp(unittest.TestCase):
     def test_hello_world(self):
-        self.assertEqual(hello_world(), "Hello World!")
+        self.assertEqual(hello_world(), "Edited Hello World!")
 
 if __name__ == "__main__":
     unittest.main()
